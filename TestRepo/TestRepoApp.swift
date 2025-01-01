68728c139e5402bc37c2b573cbb2fd2cf5a6c98e
Richard Armstrong <richarmstrong97@gmail.com>
2025-01-01 14:56:45 +0000

    Initial Commit

---
+++ TestRepo/TestRepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestRepoApp.swift
+//  TestRepo
+//
+//  Created by Richard Armstrong on 01/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct TestRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestRepoTests/TestRepoTests.swift
@@ -0,0 +1,16 @@
+//
+//  TestRepoTests.swift
+//  TestRepoTests
+//
+//  Created by Richard Armstrong on 01/01/2025.
+//
+
+import Testing
+
+struct TestRepoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
